d920d0db9d7c7255f6fc1a71bbdf6e486d134020
NNmod <nickvarvarik@gmail.com>
2023-07-01 21:13:18 +0300

    update games config

---
+++ Migrations/GameDb/20230701181103_GameDbV0.4.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeepoGuessrApi.Migrations.GameDb
+{
+    /// <inheritdoc />
+    public partial class GameDbV04 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RoundDuration",
+                value: 180);
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "RoundDuration",
+                value: 180);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RoundDuration",
+                value: 300);
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "RoundDuration",
+                value: 300);
+        }
+    }
+}
